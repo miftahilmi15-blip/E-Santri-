e5c00eee600d7ee2e852d0b391986f957fdf44ab
miftahilmi15-blip <miftahilmi15@gmail.com>
2025-11-25 13:42:42 +0700

    firebase.js
    // firebase.js (VERSI BENAR UNTUK COMPAT)
    
    const firebaseConfig = {
      apiKey: "AIzaSyD9....",
      authDomain: "e-santri-123.firebaseapp.com",
      projectId: "e-santri-123",
      storageBucket: "e-santri-123.appspot.com",
      messagingSenderId: "10928291...",
      appId: "1:10928:web:8ab..."
    };
    
    // Inisialisasi Firebase (compat)
    firebase.initializeApp(firebaseConfig);
    
    // WAJIB ADA: Auth & Firestore instance
    const auth = firebase.auth();
    const db = firebase.firestore();

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyD9....",
-  authDomain: "e-santri-123.firebaseapp.com",
-  projectId: "e-santri-123",
-  storageBucket: "e-santri-123.appspot.com",
-  messagingSenderId: "10928291...",
-  appId: "1:10928:web:8ab..."
-};
-firebase.initializeApp(firebaseConfig);
